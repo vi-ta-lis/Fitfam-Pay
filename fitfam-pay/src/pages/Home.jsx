063266d081241e41834e957e20527ed589b1b22c
Vitalis Kalu <vitaliskalu7@gmail.com>
2025-07-17 22:30:51 +0100

    added new home features

--- fitfam-pay/src/pages/Home.jsx
+++ fitfam-pay/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import Header from "../components/Header";
 import Input from "../components/Input";
 import HomeImg from "../assets/gym-1.png";
+import FindGym from "../assets/magnifyglass_11303180.png";
+import PayGym from "../assets/credit-card.png";
+import Gymming from "../assets/weight-lifting.png";
 
 function Home() {
   return (
@@ -15,6 +18,38 @@ function Home() {
         <div>
           <img src={HomeImg} alt="gym-photo" />
         </div>
+      </div>
+
+      <div id="line1">
+        <h1>How it Works</h1>
+
+        <div id="steps">
+          <div>
+            <img src={FindGym} alt="Finding Gym" />
+            <h2>Step 1:</h2>
+            <h3>Find gyms near your current location or preferred area</h3>
+          </div>
+
+          <div>
+            <img src={PayGym} alt="Finding Gym" />
+            <h2>Step 2:</h2>
+            <h3>Find gyms near your current location or preferred area</h3>
+          </div>
+          <div>
+            <img src={Gymming} alt="Finding Gym" />
+            <h2>Step 3:</h2>
+            <h3>Find gyms near your current location or preferred area</h3>
+          </div>
+        </div>
+      </div>
+      <div id="line1">
+        <h1>Why Choose Us?</h1>
+      </div>
+      <div id="line1">
+        <h1>Own a Gym? Let's Partner Up</h1>
+      </div>
+      <div id="line1">
+        <h1>Loved By Gym Goers</h1>
       </div>
     </main>
   );
